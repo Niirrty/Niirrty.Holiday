efb073e5c0f7eae6d2e083546a844be31e6bbcf4
niirrty <niirrty@gmail.com>
2018-04-21 19:21:13 +0200

    bugfix(ConditionCallbacks): The default condition callbacks IsSaturDay, IsSunday and IsMonday was wrong!

--- tests/Niirrty/Holiday/Tests/MoveConditionTest.php
+++ tests/Niirrty/Holiday/Tests/MoveConditionTest.php
@@ -11,6 +11,7 @@
 namespace Niirrty\Holiday\Tests;
 
 
+use Niirrty\Date\DateTime;
 use Niirrty\Holiday\DateMove\MoveCondition;
 use PHPUnit\Framework\TestCase;
 
@@ -18,15 +19,85 @@ use PHPUnit\Framework\TestCase;
 class MoveConditionTest extends TestCase
 {
 
-   #public function setUp() { parent::setUp(); }
+   private $callback;
+   private $condition;
+   private $acceptedRegions = [ 1, 3, 4 ];
 
-   public function testFoo()
+   public function setUp()
    {
 
-      $this->assertSame( '', '' );
+      $this->callback = function ( \DateTime $date ) : bool
+      {
+         return 4 === (int) $date->format( 'm' );
+      };
+
+      $this->condition = MoveCondition::Create( 1, $this->callback )
+                                      ->setAcceptedRegions( $this->acceptedRegions );
+
+      parent::setUp();
 
    }
 
+   public function testCreate()
+   {
+
+      $callback = function ( \DateTime $date ) : bool
+      {
+         return 0 !== ( (int) $date->format( 'w' ) );
+      };
+      $cond = MoveCondition::Create( 1, $callback );
+      $this->assertInstanceOf( MoveCondition::class, $cond );
+      $this->assertSame( 1, $cond->getMoveDays() );
+      $this->assertSame( $callback, $cond->getCondition() );
+      $this->assertTrue( $cond->matches( new DateTime( '2018-04-20 00:00:00' ) ) );
+      $this->assertFalse( $cond->matches( new DateTime( '2018-04-22 00:00:00' ) ) );
+
+   }
+   public function testOnSunday()
+   {
+
+      $cond = MoveCondition::OnSunday( 1 );
+      $this->assertInstanceOf( MoveCondition::class, $cond );
+      $this->assertFalse( $cond->matches( new DateTime( '2018-04-21 00:00:00' ) ) );
+      $this->assertTrue( $cond->matches( new DateTime( '2018-04-22 01:00:00' ) ) );
+
+   }
+   public function testOnMonday()
+   {
+
+      $cond = MoveCondition::OnMonday( 3 );
+      $this->assertInstanceOf( MoveCondition::class, $cond );
+      $this->assertTrue( $cond->matches( new DateTime( '2018-04-23 00:00:00' ) ) );
+      $this->assertFalse( $cond->matches( new DateTime( '2018-04-22 00:00:00' ) ) );
+
+   }
+   public function testOnSaturday()
+   {
+
+      $cond = MoveCondition::OnSaturday( 2 );
+      $this->assertInstanceOf( MoveCondition::class, $cond );
+      $this->assertTrue( $cond->matches( new DateTime( '2018-04-21 00:00:00' ) ) );
+      $this->assertFalse( $cond->matches( new DateTime( '2018-04-22 00:00:00' ) ) );
+
+   }
+   #public function testGetCondition() { $this->assertSame( '', '' ); }
+   #public function testGetAcceptedRegions() { $this->assertSame( '', '' ); }
+   #public function testGetMoveDays() { $this->assertSame( '', '' ); }
+   #public function testSetCondition() { $this->assertSame( '', '' ); }
+   #public function testSetAcceptedRegions() { $this->assertSame( '', '' ); }
+   #public function testSetMoveDays() { $this->assertSame( '', '' ); }
+   #public function testMatches() { $this->assertSame( '', '' ); }
+   #public function testMove() { $this->assertSame( '', '' ); }
+   #public function testIsValid() { $this->assertSame( '', '' ); }
+   #public function test() { $this->assertSame( '', '' ); }
+   #public function test() { $this->assertSame( '', '' ); }
+   #public function test() { $this->assertSame( '', '' ); }
+   #public function test() { $this->assertSame( '', '' ); }
+   #public function test() { $this->assertSame( '', '' ); }
+   #public function test() { $this->assertSame( '', '' ); }
+   #public function test() { $this->assertSame( '', '' ); }
+   #public function test() { $this->assertSame( '', '' ); }
+   #public function test() { $this->assertSame( '', '' ); }
    #public function test() { $this->assertSame( '', '' ); }
    #public function test() { $this->assertSame( '', '' ); }
    #public function test() { $this->assertSame( '', '' ); }
